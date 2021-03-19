8ca245f9ae6da267fc63323fd2e0b6c115b4f663
Petter Becker-Jostes <petterbj@outlook.dk>
2021-03-19 13:51:04 +0100

    Initial commit for db_implementation: Tidy working dir (remove swap files).

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
