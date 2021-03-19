303673bb5a610d6f6e2b0cf7953d9762d1d9aa74
Petter Becker-Jostes <petterbj@outlook.dk>
2021-03-19 14:10:59 +0100

    Redirect to frontpage.html.

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    return render_template('frontpage.html')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
