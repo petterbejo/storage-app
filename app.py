403189a4fb4fba2e279cb11efc2ae9525b01d646
Petter Becker-Jostes <petterbj@outlook.dk>
2021-03-24 13:35:48 +0100

    Test button functionality with redirect to test page.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 def get_db_connection():
     """
@@ -8,6 +8,7 @@ def get_db_connection():
     conn = sqlite3.connect('basement_db.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 
 # Add function to remove item from db here
 
@@ -26,3 +27,14 @@ def index():
     return render_template('frontpage.html', storage=items)
 
 
+@app.route('/test')
+def test():
+    return "Hello there, this is just a test."
+
+
+@app.route('/<int:id>', methods=('GET', 'POST'))
+def update(id):
+    conn = get_db_connection()
+    # conn.execute(' FROM storage WHERE id = ?', (id,))  
+    conn.close
+    return redirect(url_for('test'))
