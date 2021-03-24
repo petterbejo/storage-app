644a44bdcfb7ce5207ab58cfc5aec55960732c7b
Petter Becker-Jostes <petterbj@outlook.dk>
2021-03-24 11:56:29 +0100

    Add docstring for functions in app.py.

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import sqlite3
 from flask import Flask, render_template
 
 def get_db_connection():
+    """
+    Opens a connection to the database.
+    """
     conn = sqlite3.connect('basement_db.db')
     conn.row_factory = sqlite3.Row
     return conn
@@ -11,6 +14,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Views the front page.
+    """
     conn = get_db_connection()
     items = conn.execute('SELECT * FROM storage').fetchall()
     conn.close()
