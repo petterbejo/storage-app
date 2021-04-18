64558b6cf505e4d0a448e816dfe8e38e05e1cd5c
Petter Becker-Jostes <petterbj@outlook.dk>
2021-04-18 15:00:39 +0200

    Clean-up before making repo public.

--- init_db.py
+++ init_db.py
@@ -1,18 +1,20 @@
+"""
+Creates the database and stores the items in the provided CSV file in the database.
+"""
+
+
 import sqlite3
 import pandas as pd
 
 
-conn = sqlite3.connect('basement_db.db')
+conn = sqlite3.connect('my_db.db')
 c = conn.cursor()
 
+# Provide the path of your CSV here (as a string):
+my_csv = ' '
 
-read_storage = pd.read_csv('/home/pbj/Documents/projects/storage-app/in_storage_210319.csv')
+read_storage = pd.read_csv(my_csv))
 read_storage.to_sql('storage', conn, if_exists='replace', index=False)
-
-
-
-
-
 
 conn.commit()
 conn.close()
