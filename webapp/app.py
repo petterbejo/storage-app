a4d320ccee201a2311ab313a021eee5d3933bb6d
Petter Becker-Jostes <petterbj@outlook.dk>
2021-07-15 10:05:55 +0200

    Almost complete, only return to single category after update missing

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for, redirect
 
 db_path = Path.cwd().parent / ('database/storage_db.db')
 
+
 def get_db_connection():
     """
     Opens a connection to the database.
@@ -14,6 +15,7 @@ def get_db_connection():
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -32,18 +34,51 @@ def index():
 
 
 @app.route('/<int:id>/remove', methods=('GET', 'POST'))
-def remove_item(id):
+def remove_item(id, page='index'):
     conn = get_db_connection()
     conn.execute('UPDATE items SET Quantity = Quantity - 1 WHERE item_id = ?', (id,))
     conn.commit()
     conn.close()
-    return redirect(url_for('index')) 
+    return redirect(url_for(page))
 
 
 @app.route('/<int:id>/add', methods=('GET', 'POST'))
-def add_item(id):
+def add_item(id, page='index'):
     conn = get_db_connection()
     conn.execute('UPDATE items SET Quantity = Quantity + 1 WHERE item_id = ?', (id,))
     conn.commit()
     conn.close()
-    return redirect(url_for('index')) 
+    return redirect(url_for(page))
+
+
+@app.route('/categories')
+def category_view():
+    """
+    Lets the user view and select all available categories.
+    """
+    conn = get_db_connection()
+    items = conn.execute(
+        'SELECT * '
+        'FROM categories'
+    ).fetchall()
+    conn.close()
+    return render_template('categoryview.html', categories=items)
+
+
+@app.route('/<int:id>/category_view')
+def single_category(id):
+    """
+    Views items of a single category.
+    """
+    conn = get_db_connection()
+    items = conn.execute(
+        'SELECT item_id, categories.category, article, quantity, expiry_date '
+        'FROM items '
+        'INNER JOIN categories '
+        'ON items.category_id=categories.category_id '
+        'WHERE categories.category_id = ?', (id,)
+    ).fetchall()
+    conn.close()
+    return render_template('singlecategory.html', category=items)
+
+
